fe95cb7a5b26224a8d81a9f6543c258de3e4948b
Ammar <ammarasafi041@gmail.com>
2024-08-26 22:41:21 +0300

    login

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\api\CityController;
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\api\LoginController;
 use App\Http\Resources\CityResource;
 use App\Models\City;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,12 +20,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // return $request->user();
+    return Auth::user();
 });
 
 
-// Route::post("/login",[LoginController::class, "login"]);
-// Route::middleware("auth:sanctum")->get("/logout", [LoginController::class, "logout"]);
+Route::post("/login",[LoginController::class, "login"]);
+Route::middleware("auth:sanctum")->get("/logout", [LoginController::class, "logout"]);
 
 Route::get("/show/cities" , [CityController::class , "index"]) ;
 Route::middleware("auth:sanctum")->group(function(){
