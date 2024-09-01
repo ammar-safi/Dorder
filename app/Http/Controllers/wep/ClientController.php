e73bd6a2cbc72df87112f1be86ea4a79cd6ced68
Ammar <ammarasafi041@gmail.com>
2024-09-01 16:01:46 +0300

    wep add image to clinet V3, in view

--- app/Http/Controllers/wep/ClientController.php
+++ app/Http/Controllers/wep/ClientController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\wep;
 use App\Http\Controllers\Controller;
 use App\Models\Area;
 use App\Models\City;
+use App\Models\Image;
 use App\Models\User;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
@@ -18,7 +20,7 @@ class ClientController extends Controller
     public function __construct()
     {
         $this->middleware(["auth", "hasAccess"]);
-        $this->middleware('isAdmin')->except(["index" , "delete"]);
+        $this->middleware('isAdmin')->except(["index", "delete"]);
     }
     /**
      * Display a listing of the resource.
@@ -124,39 +126,49 @@ class ClientController extends Controller
 
     public function update(Request $request)
     {
-        // dd($request->all());
         try {
-            
+
             $validate = Validator::make(
                 $request->all(),
                 [
                     'id' => 'required|exists:users',
                     'name' => 'required|string|max:255',
-                    'email' => ['required', 'email', 'max:255', Rule::unique("users", "email")->where('type', "client")->ignore($request->id , "id")],
-                    'mobile' => ['required', 'string', 'regex:/^09[0-9]{8}$/',  Rule::unique("users", "mobile")->where('type', "client")->ignore($request->id , "id")],
+                    'email' => ['required', 'email', 'max:255', Rule::unique("users", "email")->where('type', "client")->ignore($request->id, "id")],
+                    'mobile' => ['required', 'string', 'regex:/^09[0-9]{8}$/',  Rule::unique("users", "mobile")->where('type', "client")->ignore($request->id, "id")],
                     'subscription_fees' => 'nullable|required_with:expire|numeric',
                     'expire' => 'nullable|date_format:Y-m-d',
                     'area_id' => 'nullable|exists:areas,id',
+                    'profile_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
                 ],
                 [
                     'id.required' => 'حدث خطأ , حاول مرا اخرى',
                     'id.exists' => 'حدث خطأ , حاول مرا اخرى',
+
                     'name.required' => "اسم العميل مطلوب",
+
                     'mobile.required' => 'قم الهاتف مطلوب',
                     'mobile.unique' => 'رقم الهاتف موجود مسبقا',
                     'mobile.regex' => 'الرقم غير صحيح',
+
                     'email.required' => 'البريد الالكتروني مطلوب',
                     'email.email' => 'يرجى ادخال بريد الكتروني صالح',
                     'email.unique' => 'البريد الالكتروني موجود مسبقا',
+
                     'area_id.exists' => 'المنطقة غير صحيحة , حاول مرة اخرة',
+
                     'subscription_fees.numeric' => 'عدد الطلبات يجب ان يكون رقم ',
-                    'subscription_fees.required_with' => "يجب اختيار عدد الطلبات" ,
+                    'subscription_fees.required_with' => "يجب اختيار عدد الطلبات",
+
                     'expire.date_format' => 'تاريخ انتهاء الصلاحية يجب ان يكون من الشكل : YYYY-MM-DD',
+                    'profile_image.required' => 'صورة العميل مطلوبة',
+                    'profile_image.image' => 'يجب ان يكون صورة',
+                    'profile_image.mimes' => 'صيغة الصورة غير صالحة',
+                    'profile_image.max' => 'حجم الصورة كبير',
                 ]
             );
 
 
-            
+
             if ($validate->fails()) {
                 return redirect()->back()->withInput($request->all())->withErrors($validate);
             }
@@ -170,8 +182,25 @@ class ClientController extends Controller
                 $client->subscription_fees = $request->subscription_fees;
                 $client->expire = $request->expire;
                 if ($client->save()) {
-                    return redirect()->route("clients.show")->with("success", 'تم تعديل العميل بنجاح');
-                } else {       
+                    if ($request->hasFile('profile_image')) {
+                        if ($client->image && Storage::exists('public/' . $client->image->url)) {
+                        }
+                        
+                        Storage::delete($client->image->url);
+                        dd('ammar');
+                        $file = $request->file('profile_image');
+                        $filename = time() . '_' . $file->getClientOriginalName();
+                        $path = $file->storeAs('clients', $filename , 'public');
+
+                        $image = new Image();
+                        $image->url = $path;
+                        if ($client->image()->save($image)) {
+                            return redirect()->route("clients.show")->with("success", 'تم تعديل العميل بنجاح');
+                        } else {
+                            return redirect()->back()->with("error", 'حدث خطأ اثناء  تعديل الصورة , حاول مرا اخرى');
+                        }
+                    }
+                } else {
                     return redirect()->back()->with("error", 'حدث خطأ, حاول مرا اخرى');
                 }
             } else {
